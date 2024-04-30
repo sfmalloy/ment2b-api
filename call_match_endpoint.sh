be3843c743768f88749b87148903ff7bf5b399e0
Zhen,Allen <allen_zhen@vanguard.com>
2024-04-30 18:08:47 +0000

    chore: cleanup

--- call_match_endpoint.sh
+++ call_match_endpoint.sh
@@ -11,16 +11,3 @@ session_token=$(curl -s -X GET \
 curl -X GET \
   -H "sessionToken: $session_token" \
   http://localhost:8080/match
-#!/bin/bash
-
-# MODIFY UID below to change user to match on
-
-# Call localhost:8080/login and parse sessionToken
-session_token=$(curl -s -X GET \
-  -H "uid: aaaa" \
-  http://localhost:8080/login | jq -r '.sessionToken')
-
-# Call localhost:8080/match with sessionToken
-curl -X GET \
-  -H "sessionToken: $session_token" \
-  http://localhost:8080/match
