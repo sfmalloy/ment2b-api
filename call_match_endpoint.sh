f84147e55bb51c29a4a914543b0bf9ecbe191c08
Matt Fossett <mattfoss01@gmail.com>
2024-04-30 14:58:47 -0400

    fixed sh file

--- call_match_endpoint.sh
+++ call_match_endpoint.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# MODIFY UID below to change user to match on
+# Path to the temporary cookie file
+cookie_file=$(mktemp)
 
-# Call localhost:8080/login and parse sessionToken
-session_token=$(curl -s -X GET \
+# Call localhost:8080/login and save cookies to a file
+curl -s -X GET \
   -H "uid: aaaa" \
-  http://localhost:8080/login | jq -r '.sessionToken')
+  --cookie-jar "$cookie_file" \
+  http://localhost:8080/login > /dev/null
 
-# Call localhost:8080/match with sessionToken
+# Parse the "ment2b_session" cookie value from the cookie file
+session_cookie=$(awk '/ment2b_session/{print $NF}' "$cookie_file")
+
+# Call localhost:8080/user with the "ment2b_session" cookie
 curl -X GET \
-  -H "sessionToken: $session_token" \
+  -H "Cookie: ment2b_session=$session_cookie" \
+  http://localhost:8080/user
+
+# Call localhost:8080/match with the same "ment2b_session" cookie
+curl -X GET \
+  -H "Cookie: ment2b_session=$session_cookie" \
   http://localhost:8080/match
+
+# Remove the temporary cookie file
+rm "$cookie_file"
