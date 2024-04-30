f90c91eb1a1c824cc377cd1f9900580ffde03a28
Sean Malloy <techfan321@gmail.com>
2024-04-30 10:10:06 -0400

    Add CORSMiddleware

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, Header, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from models import PostSchema
 
 import secrets
 import database as db
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
 
 @app.get("/")
 async def hello():
